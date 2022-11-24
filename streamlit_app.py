9f099bf18b436edb4a944a4848b0c2314d371303
Paul <paultsk.pt@gmail.com>
2022-11-24 11:37:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.header('Breakfast Favorites')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinacle & Rocket Smoothie')
-strealit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header(' Build Your Own Fruit Smoothie ')
 
